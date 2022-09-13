16d808bebc241311e60886533315872f351ec769
Likith Ponnanna <likith.april13@gmail.com>
2022-09-13 15:32:22 -0400

    Personal Wesite

---
+++ data/index.js
@@ -0,0 +1,74 @@
+export const defaultTheme = 'dark'; // light
+
+export const Title = 'Likith Ponnanna -  Machine Learning Engineer';
+
+export const Resume = {
+    introduction: ` 
+    def __init__():
+        first_name =  'Likith Ponnanna', 
+        last_name =   'Patrapanda Belliappa'
+        designation = 'Machine Learning Engineer'
+        company =     'Quantiphi'
+     `,
+
+    experience: ` 
+    def experience():
+        Quantiphi = {
+            Company:             'Quantiphi Inc',
+            Designation:         'Machine Learning Engineer',
+            Period:              'March 2020 to Present'
+        }
+        KodakAlaris = {
+            Company:             'Kodak Alaris - AI Foundry (acquired by Guaranteed Rate)',
+            Designation:         'Machine Learning Engineer (Co-op / Intern)',
+            Period:              'May 2019 to Dec 2019'
+        }
+        ViaggioTechnologies = {
+            Company:             'Viaggio Technologies Pvt. Ltd.',
+            Designation:         'Data Scientist / Co-Founder',
+            Period:              'Sep 2016 to June 2018'
+        }
+        return Quantiphi, KodakAlaris, ViaggioTechnologies
+    `,
+
+    skills: ` 
+    def skills():
+        Language = { 
+            C, C++, Java, Python, JavaScript, Go, CUDA.
+        }
+        MachineLearning = {
+            Frameworks         'PyTorch, TensorFlow, Scikit-learn, NVIDIA TAO, ONNX, OpenCV, MATLAB, Keras, Caffe, MLlib, NVIDIA Omniverse, Rapids'
+            Operations:        'AWS SageMaker, Google Vertex AI, MLFlow',
+            DeploymentTools:   'Docker, Kafka, Kubernetes, Deepstream, Triton Server, NVIDIA Fleet Command, NVIDIA Metropolis, TensorRT'
+        }
+        Cloud = {
+            Platforms:       'Amazon Web Services [Certified ML - Specialty], Azure, Google Cloud Platform, Heroku'
+            Database:        'NoSQL, SQLite, MongoDB, PostgreSQL, MySQL, Redis'
+            WebTechnologies: 'HTML5/CSS3, AngularJS, NodeJS, ReactJS, Spring Boot, jQuery, JavaScript, REST, gRPC, Flask'
+        }
+        return Language, MachineLearning, Cloud
+    `,
+
+    projects: ` 
+    def projects():
+        github = {url: <a target='_blank' href='https://github.com/likithponnanna'>'https://github.com/likithponnanna'</a>}
+        return github
+    `,
+
+    contact: ` 
+    def contact():
+        email    = <a target='_blank' href='mailto:contact@likithponnanna.com'>contact@likithponnanna.com</a>
+        github   = <a target='_blank' href='https://github.com/likithponnanna'>@likithponnanna</a>
+        linkedin = <a target='_blank' href='https://www.linkedin.com/in/likith-ponnanna/'>@likith-ponnanna</a>
+        return email, github, linkedin
+    `,
+
+};
+
+
+export const SocialLinks = {
+    resume: '/resume.pdf',
+    linkedin: 'https://www.linkedin.com/in/likith-ponnanna/',
+    twitter: 'https://twitter.com/likithponnanna1',
+    github: 'https://github.com/likithponnanna'
+}
